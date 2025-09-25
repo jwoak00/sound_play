9b0c5da548b355a9bfa9f47b4093fe2f8542ee3a
jwoak00 <a01051573747@gmail.com>
2025-09-25 15:37:50 +0900

    Update launch; replace sounds with test_* files

--- launch/sound_play.launch.py
+++ launch/sound_play.launch.py
@@ -11,8 +11,8 @@ def generate_launch_description():
             parameters=[{
                 'topic': '/sound_id',
                 'play_mode': 'single',          # 'single' or 'overlap'
-                'file_ids':  [0, 1, 2, 3, 4, 5],
-                'file_names': ['idle.wav', 'start.mp3', 'stop.wav', 'warn.mp3', 'tmp.mp3', 'test_0.mp3'],       # 추가 후 재빌드
+                'file_ids':  [0, 1, 2, 3],
+                'file_names': ['test_0.mp3', 'test_1.mp3', 'test_2.mp3', 'test_3.mp3'],       # 추가 후 재빌드
             }],
         )
     ])
