3b02739641d2e4d50347e2d90ee57ae6cc3bfebf
jwoak00 <a01051573747@gmail.com>
2025-09-25 16:47:49 +0900

    test

--- launch/sound_play.launch.py
+++ launch/sound_play.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'topic': '/sound_id',
-                'play_mode': 'single',          # 'single' or 'overlap'
+                'play_mode': 'single',            # 'single' or 'overlap'
                 'file_ids':  [0, 1, 2, 3],
                 'file_names': ['test_0.mp3', 'test_1.mp3', 'test_2.mp3', 'test_3.mp3'],       # 추가 후 재빌드
             }],
